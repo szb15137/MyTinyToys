eadd5cee3d1601dfe953a02ef0c59b846c55c228
Neil Zhang <siqi_zhang_2025@outlook.com>
2025-12-16 01:19:57 +0800

    1st version

---
+++ mmpool.cpp
@@ -0,0 +1,45 @@
+#include "mmpool.h"
+
+MyMmPool::MyMmPool(int num, int size = 4096) : block_num(num), free_list(nullptr) {
+    pool_size = size > sizeof(void*) ? size : sizeof(void*);
+    pool_size = align_size(pool_size, sizeof(void*));
+    expand();
+}
+MyMmPool::~MyMmPool() {
+    for (char* pool : mm_pool_list){
+        ::operator delete(pool); // Any potential memory leakage? 
+    }
+}
+
+void MyMmPool::expand(){
+    // Will only be called when free_list is empty
+    char* new_pool = static_cast<char*>(::operator new (pool_size * block_num));
+    mm_pool_list.push_back(new_pool);
+    for (int i = 0; i < block_num; i++){
+        PoolNode* ptr = reinterpret_cast<PoolNode*>(new_pool);
+        ptr -> next = free_list;
+        free_list = ptr;
+        new_pool += pool_size;
+    }
+}
+
+void* MyMmPool::allocate() {
+    //
+    if (!free_list){
+        expand();
+    }
+    void* node = static_cast<void*>(free_list);
+    free_list = free_list -> next;
+    return node;
+}
+
+void MyMmPool::deallocate(void* ptr) {
+    //
+    PoolNode* node = static_cast<PoolNode*>(ptr);
+    node -> next = free_list;
+    free_list = node;
+}
+
+inline int MyMmPool::align_size(int size, int alignment){
+    return (size + (alignment - 1)) & ~ (alignment - 1);
+}
---
+++ mmpool.h
@@ -0,0 +1,30 @@
+#include <vector>
+
+struct PoolNode
+{
+    /* data */
+    PoolNode *next;
+};
+class MyMmPool
+{
+public:
+    explicit MyMmPool(int num, int size) {};
+    ~MyMmPool();
+    MyMmPool(const MyMmPool &other) = delete;
+    MyMmPool(MyMmPool &&other) = delete;
+    MyMmPool &operator=(const MyMmPool &other) = delete;
+    MyMmPool &operator=(MyMmPool &&other) = delete;
+    void *allocate();
+    void deallocate(void* ptr);
+
+private:
+    static inline int align_size(int size, int alignment) {};
+    // void* split(void* ptr);
+    void expand();
+
+private:
+    int pool_size;
+    int block_num;
+    PoolNode* free_list;
+    std::vector<char*> mm_pool_list;
+};
